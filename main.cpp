244a380bde3b946f76f4d6b8619d22c05311e1bc
arkhodamoradi <ar.khodamoradi@gmail.com>
2014-06-24 15:19:08 -0700

    queue for test

---
+++ main.cpp
@@ -0,0 +1,66 @@
+#include <cv.h>
+#include <highgui.h>
+#include <queue>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    VideoCapture input_cap(0);
+
+    if (!input_cap.isOpened())
+    {
+        cout << "!!! Input video could not be opened" << endl;
+        return -1;
+    }
+
+    queue <Mat> frames;
+    Mat frame;
+    int i = 0;//27=ESC, 101=e
+    int limit = 30;
+    int save = 0;
+
+    while (i!=27)
+    {
+
+        if (!input_cap.read(frame))
+        {
+            break;
+        }
+        if(frames.size()>=limit)
+        {
+            frames.pop();
+            frames.push(frame.clone());
+        }
+        else
+        {
+            frames.push(frame.clone());
+        }
+        if(i == 101)
+        {
+            limit = 60;
+            save = 1;
+        }
+        if((save == 1)&&(frames.size()>=limit))
+        {
+            VideoWriter output_cap("video.avi", CV_FOURCC('M','J','P','G'), 10.0, Size(input_cap.get(CV_CAP_PROP_FRAME_WIDTH), input_cap.get(CV_CAP_PROP_FRAME_HEIGHT)), true);
+            if (!output_cap.isOpened()){
+                cout << "!!! Output video could not be opened" << endl;
+                return -1;
+            }
+            printf("number of frames saved = %d\r\n", frames.size());
+            while(!frames.empty()){
+                output_cap.write(frames.front());
+                imshow("record", frames.front());
+                waitKey(10);
+                frames.pop();
+            }
+            output_cap.release();
+            save = 0;
+        }
+        imshow("FrontCam", frame);
+        i = waitKey(10);
+    }
+    input_cap.release();
+}
