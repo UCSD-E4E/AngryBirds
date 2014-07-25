5738a6a3245534fde8e99968e82f6f073846b065
Angela To <to.angelac@gmail.com>
2014-07-25 19:45:35 +0000

    Analog Stream

---
+++ analog_stream.cpp
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include "BlackLib.h"
+
+using namespace std;
+
+int main() 
+{
+ 				   // Ground = 34
+    BlackADC *adc = new BlackADC(AIN4); // Pin 33 
+    float analog; 
+    float converted;
+
+    while (1) 
+    {
+        if (adc->fail()) 
+        {
+            cerr << "Error" << endl;
+        }
+
+        analog = adc->getNumericValue();
+        converted = adc->getParsedValue(dap3);      
+
+        cout << "analog_input: " << analog << endl;
+        cout << "converted voltage: " << converted << endl; 
+        cout << "\n" << endl;
+
+        sleep(1);
+    }
+}
+
+
+
