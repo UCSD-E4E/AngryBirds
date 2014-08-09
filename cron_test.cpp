9920a824ab237e572efb8a7ed9570448edf41936
Angela To <to.angelac@gmail.com>
2014-08-09 15:02:32 -0700

    new test

--- cron_test.cpp
+++ cron_test.cpp
@@ -6,14 +6,46 @@ using namespace std;
 
 int main()
 {
+    int count = 0;
+    int c=4;
     // Create new file to test success of cron scheduler
-    std::ofstream f("/home/ubuntu/AngryBirds/ok.txt");
-    if(!f)
-    {
-        std::cerr<<"Cannot open the output file."<<std::endl;
-        return 1;
+    ofstream f;
+    f.open("/home/ubuntu/AngryBirds/ok.txt", 
+               std::fstream::in  | 
+               std::fstream::out | 
+               std::fstream::app);
+    f << "\nSUCCESS1\n";
+    f.close();
+    while (c>=0) {
+        f.open("/home/ubuntu/AngryBirds/ok.txt", 
+                    std::fstream::in  | 
+                    std::fstream::out |
+                    std::fstream::app);
+        cout << "opened ok" << endl;
+        if(!f)
+        {
+            cerr<<"Cannot open the output file." << endl;
+            return 1;
+        }
+        while (count < 10)
+        {
+            cout << "writing to file" << endl;
+            f << "\nSUCCESS\n";
+            count++;
+            f << count;
+            f << "ok";
+
+        }
+        count = 0;
+        f.close();
+        cout << "closed ok" << endl;
+        c--;
     }
-    f<<"\nSUCCESS\n";
+    f.open("/home/ubuntu/AngryBirds/ok.txt", 
+                std::fstream::in  |
+                std::fstream::out | 
+                std::fstream::app);
+    f << "\nSUCCESS2\n";
     f.close();
 
     // NOTE: output is diverted to /var/log/syslog during cron
