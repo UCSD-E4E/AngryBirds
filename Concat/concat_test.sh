df66359ce1308a7bcb034288431e870ce618ef19
Angela To <to.angelac@gmail.com>
2014-08-26 16:26:29 +0000

    Iterates through parent img dir to concatenate img to vid

--- Concat/concat_test.sh
+++ Concat/concat_test.sh
@@ -5,7 +5,5 @@ echo "$now"
 id="/home/ubuntu/AngryBirds/SDCard/videos/$now.avi"
 echo "$id"
 
-
-
 sudo avconv -r 20 -i /home/ubuntu/AngryBirds/SDCard/images/0/img_000.jpg -b:v 1000k "$id"
-
+#sudo avconv -r 20 -i ./img_000.jpg -b:v 1000k "$id"
--- Concat/process_dirs.sh
+++ Concat/process_dirs.sh
@@ -5,6 +5,16 @@ script=/home/ubuntu/AngryBirds/Concat/concat_test.sh
 
 echo "Attempting to concat images into avi"
 find ${path:?} -type d | while read dir; do
-    (cd $dir && sh "$script")
+     echo "Processing dir: $dir"
+     echo " "
+     cd $dir
+     now=$(date +"%m-%d-%Y_%H.%M.%S")
+     echo "$now"
+     echo " "
+     id="/home/ubuntu/AngryBirds/SDCard/videos/$now.avi"
+     echo "$id"
+     echo " "
+     sudo avconv -r 20 -i "$dir"/img_000.jpg -b:v 1000k "$id"
+     sleep 1 # Sleep to prevent files from being overwritten (same timestamp name)
 done
 echo "Done"
