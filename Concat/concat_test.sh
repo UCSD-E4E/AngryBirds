29eeccd88fa2372e00f9c0c55b327f1785f4f152
Angela To <to.angelac@gmail.com>
2014-08-31 21:01:50 -0700

    Reorganied files

--- Concat/concat_test.sh
+++ Concat/concat_test.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-now=$(date +"%m-%d-%Y_%H.%M.%S")
-echo "$now"
-id="/home/ubuntu/AngryBirds/SDCard/videos/$now.avi"
-echo "$id"
+sudo avconv -r 20 -i /home/ubuntu/AngryBirds/SDCard/videos/0/img_000.jpg -b:v 1000k /home/ubuntu/AngryBirds/SDCard/test.mp4
 
-sudo avconv -r 20 -i /home/ubuntu/AngryBirds/SDCard/images/0/img_000.jpg -b:v 1000k "$id"
-#sudo avconv -r 20 -i ./img_000.jpg -b:v 1000k "$id"
+
--- concat_test.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sudo avconv -r 20 -i /home/ubuntu/AngryBirds/SDCard/videos/0/img_000.jpg -b:v 1000k /home/ubuntu/AngryBirds/SDCard/test.mp4
-
-
